e73cce36cd7e291a92e7a2132339a2fc0fb5c788
Rajnish5821Kumar <157160330+Rajnish5821Kumar@users.noreply.github.com>
2024-08-20 01:27:15 +0530

    commit changes

---
+++ src/customer/components/HomeCarosel/MainCaroselData.js
@@ -0,0 +1,19 @@
+export const mainCarousalData=[
+    {
+        image:"https://www.istockphoto.com/photo/confident-businesswoman-in-modern-office-gm1705503967-539160891?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fwoman%2F&utm_term=woman" 
+    },
+    {
+        image:"https://www.ethnicplus.in/charming-pink-zari-weaving-silk-traditional-saree-with-blouse"
+    },
+    {
+        image:"https://www.ethnicplus.in/amazing-purple-sequins-georgette-bridesmaid-lehenga-choli"
+    },
+    {
+        image:"https://www.ethnicplus.in/sabyasachi-maroon-floral-printed-organza-bridal-lehenga-choli"
+    },
+    {
+        image:"https://www.ethnicplus.in/excellent-pink-embroidered-georgette-sangeet-wear-lehenga-choli"
+    }
+  
+    
+]
